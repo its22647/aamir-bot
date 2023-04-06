0ec0818365cde6542fd2425ae169457caa31fbe0
its22647 <116485239+its22647@users.noreply.github.com>
2023-04-06 19:42:24 +0500

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -6,34 +6,34 @@ let handler = async (m, { conn, args, command, usedPrefix }) => {
 if (!args[0]) throw `*Where is the link, Example: ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/` 
 if (!args[0].match(/www.facebook.com|fb.watch/g)) throw `*Where is the link, Example: ${usedPrefix + command}* https://fb.watch/fOTpgn6UFQ/`
 try {
-await m.reply(`*Please wait downloading your video, It will take 2 - 10 min depending on the size of your videoᴏ...*`)
+await m.reply(`*Please wait, downloading your video, It will take 2 - 10 min depending on the size of your videoᴏ...*`)
 switch (command) {   
 case "facebook3": case "fb3": case "facebookdl3": case "fbdl3":                                  
 let res = await fbDownloader(args[0])
 for (let result of res.download) {
 let ur = result.url    
-await conn.sendFile(m.chat, ur, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」*', m)}
+await conn.sendFile(m.chat, ur, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɓσƭ ɓყ: ααɱเɾ 」*', m)}
 break           
 case "facebook2": case "fb2": case "facebookdl2": case "fbdl2":           
 let ress = await fg.fbdl(args[0])
 let urll = await ress.data[0].url    
-await conn.sendFile(m.chat, urll, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」*', m) 
+await conn.sendFile(m.chat, urll, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɓσƭ ɓყ: ααɱเɾ 」*', m) 
 break
 case "facebook": case "fb": case "facebookdl": case "fbdl":        
 let vio = await fetch(`https://api.violetics.pw/api/downloader/facebook?apikey=beta&url=${args[0]}`)  
 let vioo = await vio.json()
 let videovio = `${vioo.result.hd.url || vioo.result.sd.url}`
-await conn.sendFile(m.chat, videovio, `error.mp4`, '*HERE IS YOUR VIDEO \n\n「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」*', m)
+await conn.sendFile(m.chat, videovio, `error.mp4`, '*HERE IS YOUR VIDEO \n\n「 ɓσƭ ɓყ: ααɱเɾ 」*', m)
 break   
 case "facebook4": case "fb4": case "facebookdl4": case "fbdl4":           
 const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*HERE IS YOUR VIDEO \n\n「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」*', m)
+for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, '*HERE IS YOUR VIDEO \n\n「 ɓσƭ ɓყ: ααɱเɾ 」*', m)
 break          
 case "facebook5": case "fb5": case "facebookdl5": case "fbdl5":        
 let res3 = await fetch(`https://latam-api.vercel.app/api/facebookdl?apikey=brunosobrino&q=${args[0]}`)  
 let json = await res3.json()
 let url3 = await json.video
-await conn.sendFile(m.chat, url3, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɪᴋʀᴀᴛᴏs-ᴍᴅ-ᴠ𝟷 」*', m)     
+await conn.sendFile(m.chat, url3, 'error.mp4', '*HERE IS YOUR VIDEO \n\n「 ɓσƭ ɓყ: ααɱเɾ 」*', m)     
 break    
 }} catch {
 await await m.reply(`*Error, Please try these commands: (${usedPrefix}fb, ${usedPrefix}fb2, ${usedPrefix}fb3, ${usedPrefix}fb4, ${usedPrefix}fb5)*`)
